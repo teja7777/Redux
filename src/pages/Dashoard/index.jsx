a8aa46a870b71b3cd8b326771681b19a13a208f8
gleecus <teja.ulisetti@gleecus.com>
2024-11-30 22:56:44 +0530

    feat:implement dispatch action for the store

--- src/pages/Dashoard/index.jsx
+++ src/pages/Dashoard/index.jsx
@@ -1,10 +1,17 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addItem } from "../../redux/Slices/CartSlice";
 
 const Dashboard = () => {
     const cartState = useSelector(store => store.cart.items);
+    const dispatch = useDispatch();
+
+    const handleStore = () => {
+        dispatch(addItem("someRando"));
+    };
     return (
         <div>
-            This is the redux page- I want to play with-{cartState?.length}
+            <button onClick={handleStore}>ADD To Store</button>
+            <p>This is the redux page- I want to play with-{cartState?.length}</p>
         </div>
     )
 };
